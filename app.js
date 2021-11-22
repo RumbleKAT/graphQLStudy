3d06d47822c0984312eac7625489fa78dc349efd
RumbleKAT <reki318@naver.com>
2021-11-22 23:18:27 +0900

    [feat] mutation Message

--- app.js
+++ app.js
@@ -2,38 +2,66 @@ var express = require('express')
 var { graphqlHTTP } = require('express-graphql')
 var { buildSchema } = require('graphql')
 var cors = require('cors')
-const { default: RandomDie } = require('./components/RandomDie')
 
 var schema = buildSchema(`
-    type RandomDie{
-        numSides : Int!
-        rollOnce : Int!
-        roll(numRolls: Int!): [Int]
+    input MessageInput{
+        content : String
+        author : String
     }
 
-    type Mutation{
-        setMessage(message: String) : String
+    type Message{
+        id : ID!
+        content : String
+        author : String
     }
 
     type Query{
-        getDie(numSides : Int) : RandomDie
-        getMessage : String
+        getMessage(id: ID!): Message
+    }
+
+    type Mutation{
+        createMessage(input: MessageInput): Message
+        updateMessage(id: ID!, input: MessageInput): Message
     }
 `)
+
+class Message {
+    constructor(id, {content, author}) {
+      this.id = id;
+      this.content = content;
+      this.author = author;
+    }
+  }
+  
 
 var fakeDatabase = {};
 
 var root ={
-    getDie: ({numSides}) =>{
-        return new RandomDie(numSides || 6)
-    },
-    setMessage : ({message})=>{
-        fakeDatabase.message = message;
-        return message;
-    },
-    getMessage : ()=>{
-        return fakeDatabase.message;
-    }
+    getMessage: ({id}) => {
+        if (!fakeDatabase[id]) {
+          throw new Error('no message exists with id ' + id);
+        }
+        console.log(fakeDatabase)
+        return new Message(id, fakeDatabase[id]);
+      },
+      createMessage: ({input}) => {
+        // Create a random id for our "database".
+        var id = require('crypto').randomBytes(10).toString('hex');
+    
+        fakeDatabase[id] = input;
+
+        console.log(id)
+
+        return new Message(id, input);
+      },
+      updateMessage: ({id, input}) => {
+        if (!fakeDatabase[id]) {
+          throw new Error('no message exists with id ' + id);
+        }
+        // This replaces all old data, but some apps might want partial update.
+        fakeDatabase[id] = input;
+        return new Message(id, input);
+      },
 }
 
 
