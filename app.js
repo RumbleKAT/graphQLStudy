50cd69d212b2af7145306343df3e9c352c3cd1ce
RumbleKAT <reki318@naver.com>
2021-11-22 22:33:34 +0900

    [feat] apollo-server adjust

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const { graphql, buildSchema } = require('graphql')
+var express = require('express')
+var { graphqlHTTP } = require('express-graphql')
+var { buildSchema } = require('graphql')
 
 var schema = buildSchema(`
     type Query{
@@ -12,6 +14,11 @@ var root ={
     }
 }
 
-graphql(schema, '{ hello }',root).then((res)=>{
-    console.log(res);
-})
+var app = express()
+app.use('/graphql',graphqlHTTP({
+    schema : schema,
+    rootValue : root,
+    graphiql : true
+}))
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql')
